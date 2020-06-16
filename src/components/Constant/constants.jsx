e41935b9f96c7609f5356f40430bf7fbdb210e19
meghavkumar <meghavkumar92@gmail.com>
2020-06-17 04:20:18 +0530

    1. Background gif for the webpage. 2. Created a folder under src for constants file.

---
+++ src/components/Constant/constants.jsx
@@ -0,0 +1,14 @@
+//fetchLiveData.jsx
+export const FETCH_LIVEDATA_URL = 'http://192.168.56.1:5000/';
+
+//temperatureGraph.jsx
+export const TEMPERATURE_WEEK = 'http://192.168.56.1:5000/pressure';
+export const TEMPERATURE_LAST24 = 'http://192.168.56.1:5000/pressure';
+
+//pressureGraph.jsx
+export const PRESSURE_WEEK = 'http://192.168.56.1:5000/pressure';
+export const PRESSURE_LAST24 = 'http://192.168.56.1:5000/pressure';
+
+//humidityGraph.jsx
+export const HUMIDITY_WEEK = 'http://192.168.56.1:5000/pressure';
+export const HUMIDITY_LAST24 = 'http://192.168.56.1:5000/pressure';
